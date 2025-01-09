efb10b9a1df168e25e1c7baf4446932953936c08
astemo6 <jwd1208@gmail.com>
2025-01-09 18:27:33 +0800

    Update disable_password_login.sh

--- script/disable_password_login.sh
+++ script/disable_password_login.sh
@@ -7,6 +7,15 @@ sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/g' /etc/ssh/sshd_config
 # 禁用密码认证
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
+# 禁用键盘交互式认证
+sudo sed -i 's/^#\?KbdInteractiveAuthentication.*/KbdInteractiveAuthentication no/g' /etc/ssh/sshd_config
+
+# 启用证书认证
+sudo sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config
+
+# 编辑 /etc/ssh/sshd_config.d/60-cloudimg-settings.conf 文件，禁用密码登录
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
+
 # 重启 SSH 服务以使更改生效
 sudo service sshd restart
 
