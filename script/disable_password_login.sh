b7806f1f5baf6df9d2302bd5e501367f7616424f
astemo6 <jwd1208@gmail.com>
2025-01-10 10:00:52 +0800

    Update disable_password_login.sh

--- script/disable_password_login.sh
+++ script/disable_password_login.sh
@@ -1,7 +1,9 @@
-# 设置root密码（可选）
+#!/bin/bash
+
+# 设置 root 密码（可选）
 # echo root:YourRootPasswd | sudo chpasswd
 
-# 禁用root登录
+# 禁用 root 登录
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no/g' /etc/ssh/sshd_config
 
 # 禁用密码认证
@@ -19,8 +21,33 @@ sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ss
 # 重启 SSH 服务以使更改生效
 sudo service sshd restart
 
-# log清除
+# 清除日志文件
 sudo truncate -s 0 /var/log/auth.log
 sudo rm -rf /var/log/*.gz
 sudo rm -rf /var/log/*.1
 sudo rm -rf /var/log/*.0
+
+# 安装 Fail2Ban
+sudo apt update
+sudo apt install -y fail2ban
+
+# 配置 Fail2Ban
+cat <<EOF | sudo tee /etc/fail2ban/jail.local
+[DEFAULT]
+bantime = -1
+findtime = 10m
+maxretry = 10
+backend = systemd
+
+[sshd]
+enabled = true
+port = ssh
+logpath = /var/log/auth.log
+EOF
+
+# 重启 Fail2Ban 服务以加载新配置
+sudo systemctl restart fail2ban
+sudo systemctl enable fail2ban
+
+# 验证 Fail2Ban 状态
+sudo fail2ban-client status sshd
